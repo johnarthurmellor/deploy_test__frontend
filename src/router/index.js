360608bc71ee36874ffdf1a39092ed11600389f8
jam <115711598+johnarthurmellor@users.noreply.github.com>
2024-05-19 16:42:07 +0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -4,17 +4,17 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: 'deploy_test__frontend/',
       name: 'home',
       component: () => import('@/views/HomeView.vue')
     },
     {
-      path: '/register',
+      path: 'deploy_test__frontend/register',
       name: 'register',
       component: () => import('@/views/RegisterView.vue')
     },
     {
-      path: '/posts',
+      path: 'deploy_test__frontend/posts',
       name: 'posts',
       component: () => import('@/views/PostsView.vue')
     }
